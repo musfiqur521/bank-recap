eeaf0cdebd7e9fb560e701d56e28184714658f83
musfiqur521 <musfiqur521@gmail.com>
2023-02-24 22:54:24 +0600

    some value add

---
+++ JS/diposit.js
@@ -0,0 +1,28 @@
+document.getElementById('btn-deposit').addEventListener('click', function(){
+
+    const depositField = document.getElementById('deposit-field');
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
+
+    //step-3
+    depositField.value = '';
+
+    //step-4
+    const depositTotalElement = document.getElementById('deposit-total');
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+    
+    //step-5
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    depositTotalElement.innerText = newDepositTotal;
+
+    //step-6
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotalAmount = parseFloat(previousBalanceTotalString);
+
+    // step-7
+    const newBalanceTotal = previousBalanceTotalAmount + newDepositAmount;
+    balanceTotalElement.innerText = newBalanceTotal;
+
+})
